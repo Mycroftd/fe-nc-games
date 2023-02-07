a89a9ee2413c1b0fd032449000d98644ab40ae51
Mycroftd <mycroftd@hotmail.com>
2023-02-07 08:29:36 +0000

    view a review

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Reviews} from "./routes/Reviews";
+import {Review} from "./routes/Review";
 
 function App() {
 return(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Reviews />} />
+      <Route path="/review/:review_id" element={<Review/>} />
     </Routes>
   </BrowserRouter>
 )
